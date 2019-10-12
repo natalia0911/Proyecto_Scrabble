220b38d84de6e6b8429ce72d4443b700e3be8a1b
Javyre90 <56284957+Javyre90@users.noreply.github.com>
2019-10-12 10:19:57 -0600

    Add files via upload

---
+++ Scrabble.py
@@ -0,0 +1,86 @@
+#
+#
+#
+#
+
+#Importacion de librerias
+
+from tkinter import *
+
+
+#variables globales
+matriz = [
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+
+matrizM = [
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+letras=[]
+
+#Definición de Funciones
+def crearTablero():
+    global matriz
+    for i in range(0,15):
+        for j in range(len(matriz[0])):
+            casilla = Button(frame,width=2,height=1,bg="#cfc27c")
+            casilla.grid(row=i, column=j)
+    
+    
+
+def cargarBD():
+    global letras
+    try:
+        archivoVL = open("ValorLetras.txt","r")
+        linea=archivoVL.readline()
+
+        while linea!="":
+            letras.append(linea.split(sep=","))
+            linea=archivoVL.readline()
+        print(letras)
+        archivoVL.close
+        
+        return 
+    except:
+        return "Ocurrió un error"
+    
+    
+
+
+#Programa Principal
+
+ventana = Tk()
+ventana.title("PalabraCraft")
+frame=Frame(ventana,bg="blue")
+frame.grid(column=0,row=0)
+
+crearTablero()
+cargarBD()
+
+ventana.mainloop()
